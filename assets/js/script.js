10f6475a931911571ad14b5b1551474be71be97e
Fernando Almeida <nando.lalmeida@gmail.com>
2022-03-31 15:09:01 -0400

    adding initial html and css styles

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+function findTrailBtn() {
+    var getFindTrailBtnElement = document.querySelector('.find-trail-btn');
+    var getFindTrailBtnElement = getFindTrailBtnElement.textContent = "clicked";
+}
