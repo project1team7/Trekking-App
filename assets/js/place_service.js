54af3f054c4f2f52ab09dd8910c43ab8f99582f5
Fernando Almeida <nando.lalmeida@gmail.com>
2022-04-07 12:04:34 -0400

    added getAdrres function, which will be called by the find trails button

--- assets/js/place_service.js
+++ assets/js/place_service.js
@@ -4,14 +4,34 @@
 let map;
 let service;
 let infowindow;
+let searchQuery = document.getElementById('address').value;
+
+function getAddress() {
+    if (!searchQuery) {
+        searchQuery = 'trail11111';
+    }
+    if (searchQuery) {
+        searchQuery = document.getElementById('address').value;
+        trails = "+trails";
+        searchQuery = searchQuery.concat(trails);
+        console.log("not null", searchQuery)
+    } 
+    if (searchQuery === null || searchQuery === "") {
+        searchQuery = 'test3333';
+    }
+
+
+    initMap(searchQuery);
+}
+
+
+
 
 function initMap() {
     // make these coords update dynamically
     const initLocation = new google.maps.LatLng(-33.867, 151.195);
-    let searchQuery = document.getElementById('address').value;
-    if (searchQuery === null || searchQuery === "") {
-        searchQuery = 'trails';
-    };
+    console.log("inside initmap", searchQuery);
+
     infowindow = new google.maps.InfoWindow();
     map = new google.maps.Map(document.getElementById("map"), {
         center: initLocation,
@@ -22,8 +42,12 @@ function initMap() {
         query: searchQuery,
         fields: ['name', 'location'],
     };
+    console.log("This is the request", request)
+
 
     service = new google.maps.places.PlacesService(map);
+    console.log("not nul3l", request)
+
     service.textSearch(request, (results, status) => {
         console.log("Here are the first 20 trails:", results)
         if (status === google.maps.places.PlacesServiceStatus.OK && results) {
